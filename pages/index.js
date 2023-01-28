e936ddca40ca7155353a011e5f73a19d9a6be0cc
Vinicius Mansur <54820485+vinamansur@users.noreply.github.com>
2023-01-27 22:34:13 -0500

    building app

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Weather from '../components/Weather'
-import Bikeshare from '../components/bikeshare'
+import Bikeshare from '../components/Bikeshare'
 import Bikestats from '../components/Bikestats'
 
 export default function Home() {
