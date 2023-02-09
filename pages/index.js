fcc01d192d0272347a8336192a6cd4a1f9e09c2c
Vinicius Mansur <54820485+vinamansur@users.noreply.github.com>
2023-02-08 22:22:16 -0500

    ttc on top of bikeshare

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Weather from '../components/Weather'
-import Bikeshare from '../components/Bikeshare'
-import Ttc from '../components/Ttc'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import Weather from "../components/Weather";
+import Bikeshare from "../components/Bikeshare";
+import Ttc from "../components/Ttc";
 
 export default function Home() {
   return (
     <>
-    <h1> Weather now in Toronto - ON</h1>
-    <Weather />
-    <br/>
-    <h1> Bike Share Toronto - info</h1>
-    <Bikeshare />
-    <br/>
-    <h1> TTC info</h1>
-    <Ttc />
+      <h1> Weather now in Toronto - ON</h1>
+      <Weather />
+      <br />
+      <h1> TTC info</h1>
+      <Ttc />
+      <br />
+      <h1> Bike Share Toronto - info</h1>
+      <Bikeshare />
     </>
-  )
+  );
 }
