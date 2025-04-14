af35142900c500e5c18e9b537b68c7c694daff9f
Vinicius Mansur <54820485+vinamansur@users.noreply.github.com>
2025-04-14 13:50:08 -0400

    fixes bus rotation angle

--- components/Map.js
+++ components/Map.js
@@ -1,7 +1,7 @@
 import useSWR from "swr";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { icon } from "leaflet";
-import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import "leaflet-rotatedmarker";
 
 // getting fast-xml-parser package
@@ -30,6 +30,7 @@ export default function Map(props) {
   }
   const [userLat, setUserLat] = useState();
   const [userLong, setUserLong] = useState();
+  const markerRef = useRef();
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -41,6 +42,13 @@ export default function Map(props) {
     }
   }, [navigator.geolocation]);
 
+  useEffect(() => {
+    if (markerRef.current && vehicleData) {
+      const marker = markerRef.current;
+      marker.setRotationAngle(vehicleData.heading); // <-- This triggers the rotation update
+    }
+  }, [vehicleData?.heading]);
+
   // fetching vehicle location data
   const { data } = useSWR(
     "https://webservices.umoiq.com/service/publicXMLFeed?command=vehicleLocations&a=ttc&r=82&t=0",
@@ -50,7 +58,7 @@ export default function Map(props) {
 
   //parsing from XML to JSON
   const parser = new XMLParser(options);
-  if(data) {
+  if (data) {
     const jsonData = parser.parse(data);
     if (jsonData.body.vehicle) {
       const latitude = jsonData.body.vehicle.a_lat;
@@ -58,32 +66,39 @@ export default function Map(props) {
       const vehicleId = jsonData.body.vehicle.a_id;
       const lastUpdate = jsonData.body.vehicle.a_secsSinceReport;
       const heading = jsonData.body.vehicle.a_heading;
-      
+
       return (
         <MapContainer
-        center={[43.68192607423363, -79.38021893174823]}
-        zoom={15}
-        scrollWheelZoom={true}
+          center={[43.68192607423363, -79.38021893174823]}
+          zoom={15}
+          scrollWheelZoom={true}
         >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <br><a href="https://www.flaticon.com/free-icons/location" title="location icons">Icons created by Pixel perfect - Flaticon</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <br><a href="https://www.flaticon.com/free-icons/location" title="location icons">Icons created by Pixel perfect - Flaticon</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
 
-        {/* user position marker */}
-        {userLat && (
-          <Marker position={[userLat, userLong]} icon={locationIcon}></Marker>
+          {/* user position marker */}
+          {userLat && (
+            <Marker position={[userLat, userLong]} icon={locationIcon}></Marker>
           )}
-        {/* Vehicle marker */}
-        <Marker position={[latitude, longitude]} icon={busIcon} rotationAngle={heading}>
-          <Popup>
-            Vehicle ID: {vehicleId}
-            <br />
-            Last update: {lastUpdate} seconds ago
-          </Popup>
-        </Marker>
-      </MapContainer>
-    );
+
+          {/* Vehicle marker */}
+          <Marker
+            ref={markerRef}
+            position={[latitude, longitude]}
+            icon={busIcon}
+            rotationAngle={heading}
+            rotationOrigin="center"
+          >
+            <Popup>
+              Vehicle ID: {vehicleId}
+              <br />
+              Last update: {lastUpdate} seconds ago
+            </Popup>
+          </Marker>
+        </MapContainer>
+      );
+    }
   }
-}
 }
